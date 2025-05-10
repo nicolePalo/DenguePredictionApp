715bfbed4b9bcc9ecdcb884c12725bad6d962b5a
Nicole Palo <nclarissepalo@gmail.com>
2025-05-11 00:30:11 +0800

    created frontend pages

--- MLApp/views.py
+++ MLApp/views.py
@@ -39,12 +39,12 @@ def diagnose(request):
 		mydata=request.data
 		unit=np.array(list(mydata.values()))
 		unit=unit.reshape(1,-1)
-		scalers=joblib.load("/Users/sahityasehgal/Documents/Coding/DjangoApiTutorial/DjangoAPI/MyAPI/scalers.pkl")
+		scalers=joblib.load(f'{PATH}')
 		X=scalers.transform(unit)
 		y_pred=mdl .predict(X)
 		y_pred=(y_pred>0.58)
 		newdf=pd.DataFrame(y_pred, columns=['Status'])
-		newdf=newdf.replace({True:'Approved', False:'Rejected'})
-		return JsonResponse('Your Status is {}'.format(newdf), safe=False)
+		newdf=newdf.replace({True:'positive', False:'negative'})
+		return JsonResponse('The clinical parameters indicate {} presence of dengue virus.'.format(newdf), safe=False)
 	except ValueError as e:
 		return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
